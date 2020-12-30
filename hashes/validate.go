102454c6e533d9ec2e119e9026dd75c029c005c0
Nex <nex@nex.sx>
2020-12-30 16:39:11 +0100

    Added functions to validate hashes

---
+++ hashes/validate.go
@@ -0,0 +1,29 @@
+// This file is part of go-savetime.
+// Copyright (c) 2021 Claudio Guarnieri
+// See the file 'LICENSE' for copying permission.
+
+package hashes
+
+import (
+	"regexp"
+)
+
+func ValidateMD5(target string) bool {
+    rxp := regexp.MustCompile(`[a-fA-F0-9]{32}`)
+    return rxp.MatchString(target)
+}
+
+func ValidateSHA1(target string) bool {
+    rxp := regexp.MustCompile(`[a-fA-F0-9]{40}`)
+    return rxp.MatchString(target)
+}
+
+func ValidateSHA256(target string) bool {
+    rxp := regexp.MustCompile(`[a-fA-F0-9]{64}`)
+    return rxp.MatchString(target)
+}
+
+func ValidateSHA512(target string) bool {
+    rxp := regexp.MustCompile(`[a-fA-F0-9]{128}`)
+    return rxp.MatchString(target)
+}
