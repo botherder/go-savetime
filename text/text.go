38d58a15acb7438fab7f957345e19028c6dd7062
Nex <nex@nex.sx>
2021-01-01 15:51:10 +0100

    Added TextContains

---
+++ text/text.go
@@ -0,0 +1,22 @@
+// This file is part of go-savetime.
+// Copyright (c) 2021 Claudio Guarnieri
+// See the file 'LICENSE' for copying permission.
+
+package slices
+
+import (
+	"strings"
+)
+
+func TextContains(text, pattern string) bool {
+	pattern = strings.ToLower(pattern)
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		line = strings.ToLower(line)
+		if strings.Contains(line, pattern) {
+			return true
+		}
+	}
+
+	return false
+}
