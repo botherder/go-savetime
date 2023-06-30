298da990581e813817a641f79e63954627d44d3d
Nex <nex@nex.sx>
2023-06-30 14:27:01 +0200

    Added CopyDir function in files package

--- files/copy.go
+++ files/copy.go
@@ -1,12 +1,46 @@
 package files
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
-// Copy a file from the source to the destination paths.
-func Copy(srcPath, dstPath string) error {
+// CopyDir recursively copies a directory from source to destination.
+func CopyDir(srcDir, dstDir string) error {
+	err := os.MkdirAll(dstDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	files, err := os.ReadDir(srcDir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		srcPath := filepath.Join(srcDir, file.Name())
+		dstPath := filepath.Join(dstDir, file.Name())
+
+		if file.IsDir() {
+			err := CopyDir(srcPath, dstPath)
+			if err != nil {
+				fmt.Println("Error copying directory:", err)
+			}
+		} else {
+			err := CopyFile(srcPath, dstPath)
+			if err != nil {
+				fmt.Println("Error copying file:", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+// CopyFile copies a file from the source to the destination paths.
+func CopyFile(srcPath, dstPath string) error {
 	srcHandle, err := os.Open(srcPath)
 	if err != nil {
 		return err
@@ -25,4 +59,9 @@ func Copy(srcPath, dstPath string) error {
 
 	err = dstHandle.Sync()
 	return err
+}
+
+// Copy is kept for retro-compatibility.
+func Copy(srcPath, dstPath string) error {
+	return CopyFile(srcPath, dstPath)
 }
